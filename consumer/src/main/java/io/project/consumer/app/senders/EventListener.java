0159a5c2a753731bf43d72631c5ca16c22b8d941
lilhovhann <lilith.hovhann@gmail.com>
2021-12-15 20:59:17 +0400

    simplified

--- consumer/src/main/java/io/project/consumer/app/senders/EventListener.java
+++ consumer/src/main/java/io/project/consumer/app/senders/EventListener.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class EventListener {
 
     
-    @KafkaListener(topics = {"event"})
+    @KafkaListener(topics = {"urish"})
     public void order(final @Payload String message
     ) {
         log.info("Received From Topic order: " + message);
